1602bdd508881b666ac8bb8f8119b53e4e508591
olabulau <117658045+olabulau@users.noreply.github.com>
2022-12-19 17:20:50 +0300

    Add files via upload

---
+++ pygame/украшение игры/2.py
@@ -0,0 +1,90 @@
+import pygame
+import os
+import random
+
+
+pygame.init()
+WIDTH = 200
+HEIGHT = 250
+screen_size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(screen_size)
+FPS = 50
+all_sprites = pygame.sprite.Group()
+clock = pygame.time.Clock()
+GRAVITY = 0.25
+screen_rect = (0, 0, WIDTH, HEIGHT)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    # if colorkey is not None:
+    if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+    image = image.convert_alpha()
+    return image
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    # количество создаваемых частиц
+    particle_count = 20
+    # возможные скорости
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
+
+
+# выключить курсор мыши
+pygame.mouse.set_visible(0)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.KEYDOWN:
+            # создаём частицы по щелчку мыши
+            create_particles(pygame.mouse.get_pos())
+
+    all_sprites.update()
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
